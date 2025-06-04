6fdc93211c1a1037a3557a8c1ee140bab94e76c2
Кирило Ковтун <145320125+Kiril461@users.noreply.github.com>
2025-06-04 17:51:55 +0300

    Create Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center">
+      <p>Розроблено [Ваше ім’я], 2025</p>
+      <div className="space-x-4">
+        <a href="https://facebook.com" className="hover:underline">Facebook</a>
+        <a href="https://twitter.com" className="hover:underline">Twitter</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
