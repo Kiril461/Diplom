0f396b3f551d0678bb5269020f28701a9783a63c
Кирило Ковтун <145320125+Kiril461@users.noreply.github.com>
2025-06-17 21:39:15 +0300

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,9 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\FeedbackController;
 
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{id}', [BookController::class, 'show']);
-Route::post('/feedback', [FeedbackController::class, 'store']);
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/books/{id}', [BookController::class, 'show'])->name('books.show');
+Route::post('/feedback', [FeedbackController::class, 'store'])->name('feedback.store');
